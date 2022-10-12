6d24fc73276780daaf335adede147e9d42b640f8
charilaouc <charilaou.c@gmail.com>
2022-10-12 17:07:22 +0300

    jwt token change

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 //export const API_URL = "https://api.strapi-invoice.refine.dev";
 //export const TOKEN_KEY = "strapi-jwt-token";
 export const API_URL = "http://localhost:1337";
-export const TOKEN_KEY = "d8f43a3f5563294ca6f65cb6ebe31dc5dc0691563ac8208f05e72a854831166ad0fd959cc6b3daac8484bf9293344f9d63381cb40d470236d456635281aceb930405270b0afaeca29050dec8de2ec70db21d9b6bb82e2f4e3292e78630f8972800593613a86b648e884b878b5ed86d3ee23ed57118761ec8191691fb6907bfc0";
+export const TOKEN_KEY = "PEBR+2slDx8GftKllEQX1w==";
 //[2022-10-11 15:03:54.415] info: The Users & Permissions plugin automatically generated a jwt secret and stored it in .env under the name JWT_SECRET.
